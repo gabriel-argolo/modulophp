ab6533b773a1f83e8d4731c24419c9f660a032cc
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2022-01-27 10:57:59 -0300

    Exercicio 1 php

---
+++ php1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$character = ['Nome => Gabriel', 'Idade => 25', 
+$atributos = ['Força => 40', 'Agilidade => 43', 'Destreza => 60'],
+$vida = 430, $mana = 900];
+
+echo("$character[0], $character[1]
+</br> 
+
+Atributos: $atributos[0], $atributos[1], $atributos[2]");
+echo '<hr>';
+
+echo("Stats:
+</br>
+Vida: $character[3], Mana: $character[4]");
+
+?>
