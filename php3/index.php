4869051ad2ded6fbef3c21b3bb19270b97225ea9
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2022-02-03 20:02:05 -0300

    Atualização do modulo 3 e conclusao do exercicio

--- php3/index.php
+++ php3/index.php
@@ -26,9 +26,43 @@ function dividir($valor) {
         dividir($metade);
     }
 }
-
 echo $exemplo = dividir(100); 
 
-$numeros = [10,20,30,35,40];
+echo '<hr>';
+/*echo date('d/m/y h:i:s');
 
-filtrados = array_filter;
+$data = '2020-03-21';
+$time  = strtotime($data);
+echo date('d-m-y', $time);
+*/
+echo '<hr>';
+
+function retornaData($data){
+$time = strtotime($data);
+$d = date('N', $time);
+    switch ($d) {
+        case '1':
+            echo 'Segunda-feira';
+            break;
+        case '2':
+            echo 'Terça-feira';
+            break;
+        case '3':
+            echo 'Quarta-feira';
+            break;
+        case '4':
+            echo 'Quinta-feira';
+            break;
+        case '5':
+            echo 'Sexta-feira';
+            break;
+        case '6':
+            echo 'Sabado';
+            break;
+        case '7':
+            echo 'Domingo';
+            break;
+    }
+}
+$data = ('2022-02-03');
+retornaData($data);
