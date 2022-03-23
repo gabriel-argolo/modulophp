43ef048830119d7bd69dec231bbb319220e5737c
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2022-03-23 14:46:27 -0300

    Projeto de cadastro de usuario com email criado e finalizado sem Orientaçao a objetos

---
+++ php7cadastro/adicionar.php
@@ -0,0 +1,14 @@
+<h1>ADICIONAR USUARIO</h1>
+<form method="POST" action="adicionar_action.php">
+    <label>
+        NOME:<br/>
+        <input type="text" name="name"/>
+    </label></br></br>
+
+    <label>
+        EMAIL:<br/>
+        <input type="email" name="email"/>
+    </label></br></br>
+
+    <input type="submit" value="ADICIONAR">
+</form>
